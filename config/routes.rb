56798fbd29c71a4acafd90ba4da8a2717be4bc53
noritakasawada <snp.inc.info@gmail.com>
2021-07-28 14:28:24 +0900

    firstcommit

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+ root to: redirect('/todos')
+
+ get 'todos', to: 'sites#index'
+ get 'todos/new', to: 'sites#index'
+ get 'todos/:id/edit', to:'sites#index'
+
+ namespace :api do
+  namespace :v1 do 
+    delete '/todos/destroy_all', to: 'todos#:_all'
+    resources :todos, only: %i[index show create update destroy]
+  end
+ end
+
+end
